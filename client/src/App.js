a3fb52a3077b891f68fa376a6691fb8752670156
Rhoda P <rhodaphillipsosei@gmail.com>
2022-07-27 21:07:55 -0400

    client build, home title

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HomeTitle> 
+      STITCH
+    </HomeTitle>
   );
 }
 
 export default App;
+
+
+const HomeTitle = styled.div`
+border: 4px solid red;
+height: 300px;
+width: 99.6vw;
+display: flex;
+justify-content: center;
+align-items: center;
+font-family: Comorant;
+font-size: 80px;
+letter-spacing: 10px;
+`
